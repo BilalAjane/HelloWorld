0db068339489389297a1a1c10b8523f5b2c09969
Bilal A <bila0765@stud.kea.dk>
2021-03-05 15:11:41 +0100

    First Commit

---
+++ src/test/java/com/ajane/helloworld/HelloworldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ajane.helloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloworldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
